e897525c0efc322948f2a25248cf8c2f6a06d494
zoet24 <zt12542@my.bristol.ac.uk>
2021-03-13 16:31:18 +0000

    Initial set-up of Django framework

---
+++ ob_covers/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ob_covers project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ob_covers.settings')
+
+application = get_wsgi_application()
