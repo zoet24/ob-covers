4e2a0baf957e91f49e94917a0508b74eaced19ff
zoet24 <zt12542@my.bristol.ac.uk>
2021-03-18 15:25:46 +0000

    Add add to bag functionality

--- ob_covers/urls.py
+++ ob_covers/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
