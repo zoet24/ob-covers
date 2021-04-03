51967bdab6b963c0cd93be88a8b07e5dff624c62
zoet24 <zt12542@my.bristol.ac.uk>
2021-04-03 16:09:01 +0000

    Remove secret key and added in config vars in Heroku

--- ob_covers/settings.py
+++ ob_covers/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-1&#)_d*@7*hmcby+^125lzv8s5b)f#zy6^a_lvr+u#c#cw4c)'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ob-covers.herokuapp.com', 'localhost']
 
