5762caad24dd2ce4f733b5d170ddee36064cdfc7
zoet24 <zt12542@my.bristol.ac.uk>
2021-04-14 12:12:06 +0000

    Streamline cover swatch code and remove css styling

--- home/views.py
+++ home/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+from products.models import Product, Colour
+
 # Create your views here.
 
 
 def index(request):
     # A view to return the home page
-    return render(request, 'home/index.html')
+    products = Product.objects.all()
+    colours = Colour.objects.all()
+
+    context = {
+        'products': products,
+        'colours': colours,
+    }
+
+    return render(request, 'home/index.html', context)
