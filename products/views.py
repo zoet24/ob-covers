d2f68c88b7b33c013b554162bbde8c38a222460f
zoet24 <zt12542@my.bristol.ac.uk>
2021-05-04 21:59:39 +0000

    Add no image if statements and update product tiles css

--- products/views.py
+++ products/views.py
@@ -64,6 +64,10 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'range':
                 sortkey = 'range__name'
+            if sortkey == 'style':
+                sortkey = 'style__name'
+            if sortkey == 'colour':
+                sortkey = 'colour__friendly_name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
