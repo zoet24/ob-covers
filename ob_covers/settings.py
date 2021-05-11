77006903be39bec9d92b61c73c1acdb3fe2fa670
zoet24 <zt12542@my.bristol.ac.uk>
2021-05-11 11:50:31 +0000

    Create new postgres database

--- ob_covers/settings.py
+++ ob_covers/settings.py
@@ -121,10 +121,6 @@ WSGI_APPLICATION = 'ob_covers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.parse('POSTGRES')
-# }
-
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
